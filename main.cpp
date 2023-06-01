e8a1454062cbee8e498b948ca8aa671f846157b9
Yun Jang <jang.yunj7@gmail.com>
2023-05-31 23:12:02 -0400

    sum

--- main.cpp
+++ main.cpp
@@ -1,18 +1,13 @@
-class Solution
+#include <iostream>
+
+int main()
 {
-public:
-    bool isAnagram(string s, string t)
-    {
-        if (s.length() != t.length())
-        {
-            return false;
-        }
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if (s == t)
-        {
-            return true;
-        }
-        return false;
-    }
-};
+    int first_num, second_num, sum;
+
+    std::cout << "Enter 2 numbers to add: ";
+    std::cin >> first_num >> second_num;
+
+    sum = first_num + second_num;
+
+    std::cout << first_num << " + " << second_num << " = " << sum;
+}
