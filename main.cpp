815b5d49595211c464c6d9da266ffbec48fe9e42
Yun Jang <jang.yunj7@gmail.com>
2023-06-01 22:34:00 -0400

    data types

--- main.cpp
+++ main.cpp
@@ -2,12 +2,31 @@
 
 int main()
 {
-    int first_num, second_num, sum;
+    int age = 21;
+    int year = 1990;
+    int days = 7.5;
 
-    std::cout << "Enter 2 numbers to add: ";
-    std::cin >> first_num >> second_num;
+    std::cout << days << '\n';
 
-    sum = first_num + second_num;
+    double price = 19.99;
+    double gpa = 2.5;
+    double temperature = 21.4;
 
-    std::cout << first_num << " + " << second_num << " = " << sum;
+    std::cout << price;
+
+    char grade = 'A';
+    char initial = 'B';
+    char currency = '$';
+    std::cout << currency;
+
+    bool student = true;
+    bool power = false;
+    bool forSale = true;
+
+    std::string name = "bro";
+    std::string food = "pizza";
+
+    std::cout << "hello " << name;
+
+    return 0;
 }
