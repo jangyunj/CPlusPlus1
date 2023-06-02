5f7857f364cde49e309fcf223a8711440c4ce6a8
Yun Jang <jang.yunj7@gmail.com>
2023-06-01 22:51:23 -0400

    namespace2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+// using std::cout;
+// using std::string;
 
-// different namespace can have same named variables
+// namespace provides a solution for preventing name conflicts in large projects; allows identically named entities in diff namespaces
 namespace first
 {
     int x = 1;
