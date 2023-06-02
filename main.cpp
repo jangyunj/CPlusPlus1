d04fb33ff1ca57869147284df92f414e7d5817db
Yun Jang <jang.yunj7@gmail.com>
2023-06-01 22:38:21 -0400

    const

--- main.cpp
+++ main.cpp
@@ -2,31 +2,10 @@
 
 int main()
 {
-    int age = 21;
-    int year = 1990;
-    int days = 7.5;
+    const double PI = 3.14; // const should be uppercased
+    const int LIGHT_SPEED = 299792458;
+    double radius = 10;
+    double circumference = 2 * PI * radius;
 
-    std::cout << days << '\n';
-
-    double price = 19.99;
-    double gpa = 2.5;
-    double temperature = 21.4;
-
-    std::cout << price;
-
-    char grade = 'A';
-    char initial = 'B';
-    char currency = '$';
-    std::cout << currency;
-
-    bool student = true;
-    bool power = false;
-    bool forSale = true;
-
-    std::string name = "bro";
-    std::string food = "pizza";
-
-    std::cout << "hello " << name;
-
-    return 0;
+    std::cout << circumference << "cm";
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+
+    std::cout << circumference << "cm"
