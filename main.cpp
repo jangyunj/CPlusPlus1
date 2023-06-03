8abb069cf7ce2ff2eac5cc2f36edf43dc01d439c
Yun Jang <jang.yunj7@gmail.com>
2023-06-03 19:03:00 -0400

    userInput

--- main.cpp
+++ main.cpp
@@ -1,19 +1,13 @@
-// Type conversion - conversion of one data type to another
-// Implicit - automatic
-// Explicit - Precede value with new data type using parenthesis
-
 #include <iostream>
+
+// cout << (insertion operator)
+// cin >> (extraction operator)
 
 int main()
 {
-    double x = (int)3.14; // Explicitly cast as int
-    std::cout << x;
+    std::string name;
+    std::cout << "What is your name? ";
+    std::cin >> name; // stores in the variable "name"
 
-    std::cout << (char)100;
-    //------------------------------------------------//
-    int correct = 8;
-    int questions = 10;
-
-    double score = correct / (double)questions * 100; // Useful in cases of integer division
-    std::cout << score << "%";
+    std::cout << "Hello " << name;
 }
