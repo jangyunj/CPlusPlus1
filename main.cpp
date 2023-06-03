c92ddad4e73f8c1002b45a860f32a6e7a8ad5822
Yun Jang <jang.yunj7@gmail.com>
2023-06-02 22:47:07 -0400

    using

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <vector>
+//---Reserved keyword used to create additional name for another data type
+//--Only use when there is a clear benefit
 
-typedef std::string text_t;
-typedef int number_t;
+// typedef std::string text_t;
+// typedef int number_t;
+
+using text_t = std::string;
+using number_t = int;
 
 int main()
 {
