64b4803c02f4dc64b7104bf6af81880577d01577
Yun Jang <jang.yunj7@gmail.com>
2023-06-03 19:21:15 -0400

    cmath

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
+#include <cmath>
 
 // cout << (insertion operator)
 // cin >> (extraction operator)
 
 int main()
 {
-    double x = 3;
+    double x = 3.14;
     double y = 4;
     double z;
 
-    z = std::max(x, y);
-    z = std::min(x, y);
+    z = pow(3, 1);
+    z = sqrt(9);
+    z = abs(-3);
+    z = round(x);
+    z = ceil(x);  // always rounds UP
+    z = floor(x); // always rounds DOWN
     std::cout << z;
 }
