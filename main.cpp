2b221053e25fcbd3fe7be89167136d5e661bc90a
Yun Jang <jang.yunj7@gmail.com>
2023-06-04 16:51:40 -0400

    for loop

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
+// For loop - use when you want something done for specified amount of times
 #include <iostream>
 
 int main()
 {
-    // Do while - do something first, THEN repeat again if condition is true
-    // Ex. at the end of the game you've played, ask the user if they want to play again.
-    int number;
-    do
+    for (int i = 1; i <= 10; i++)
     {
-        std::cout << "Enter a positive #: ";
-        std::cin >> number;
-    } while (number < 0);
+        std::cout << i << '\n';
+    }
 
-    std::cout << "The # is: " << number;
+    for (int i = 10; i >= 0; i--)
+    {
+        std::cout << i << '\n';
+    }
 }
