2ff3c1cb244a9cf5f07855eceef25a9ddfed6e09
Yun Jang <jang.yunj7@gmail.com>
2023-06-03 23:01:45 -0400

    if else

--- main.cpp
+++ main.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
-#include <cmath>
 
 int main()
 {
-    double A;
-    double B;
-    double C;
+    int age;
+    std::cout << "Enter your age: ";
+    std::cin >> age;
 
-    std::cout << "Enter side A: ";
-    std::cin >> A;
-
-    std::cout << "Enter side B: ";
-    std::cin >> B;
-
-    // a = pow(a, 2);   //reassign values
-    // b = pow(b, 2);
-    // c = sqrt(a + b);
-
-    //---OR---
-    C = sqrt(pow(A, 2) + pow(B, 2));
-
-    std::cout << "side C is: " << C;
+    if (age >= 100)
+    {
+        std::cout << "You are too old" << '\n';
+    }
+    if (age >= 18)
+    {
+        std::cout << "Welcome to the site";
+    }
+    else if (age < 0)
+    {
+        std::cout << "You have not been born yet";
+    }
+    else
+    {
+        std::cout << "You are under 18";
+    }
 }
