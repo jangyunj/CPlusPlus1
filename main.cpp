bf831d88591b3b0160fa8dabae01b685fa46ec30
Yun Jang <jang.yunj7@gmail.com>
2023-06-04 16:38:39 -0400

    while loop

--- main.cpp
+++ main.cpp
@@ -3,14 +3,11 @@
 int main()
 {
     std::string name;
+    while (name.empty())
+    {
+        std::cout << "Enter your name: ";
+        std::getline(std::cin, name);
+    }
 
-    std::cout << "Enter your name: ";
-    std::getline(std::cin, name);
-
-    // name.at(0);
-    // name.insert(0, "@");
-    // name.find(' ');
-    name.erase(0, 3);
-
-    std::cout << name;
+    std::cout << "Hello " << name;
 }
