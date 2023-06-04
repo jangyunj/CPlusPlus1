ec7338aea8494f6174af1d2db3d3533cc538e778
Yun Jang <jang.yunj7@gmail.com>
2023-06-04 16:22:00 -0400

    string method2

--- main.cpp
+++ main.cpp
@@ -3,22 +3,11 @@
 int main()
 {
     std::string name;
+
     std::cout << "Enter your name: ";
-    std::getline(std::cin >> std::ws, name);
+    std::getline(std::cin, name);
 
-    if (name.length() > 12)
-    {
-        std::cout << "Your name cannot be over 12 characters";
-    }
-    else if (name.empty())
-    {
-        std::cout << "You did not enter your name";
-    }
-    else
-    {
-        std::cout << "Welcome " << name;
-    }
+    name.append("@gmail.com");
+
+    std::cout << "Your email is now " << name;
 }
-
-// other string methods
-// name.clear()
