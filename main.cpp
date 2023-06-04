381e26e4ba424710cf071c0e5b090bdf7d935b60
Yun Jang <jang.yunj7@gmail.com>
2023-06-04 17:02:35 -0400

    nested loops rectangle

--- main.cpp
+++ main.cpp
@@ -1,14 +1,25 @@
-// Break - break out of the loop
-// Continue- skip current iteration
 #include <iostream>
 
 int main()
 {
-    for (int i = 1; i <= 3; i++)
+    int rows;
+    int columns;
+    char symbol;
+
+    std::cout << "How many rows?: ";
+    std::cin >> rows;
+
+    std::cout << "How many columns: ";
+    std::cin >> columns;
+
+    std::cout << "Enter your symbol: ";
+    std::cin >> symbol;
+
+    for (int i = 1; i <= rows; i++)
     {
-        for (int j = 1; j <= 10; j++)
+        for (int j = 1; j <= columns; j++)
         {
-            std::cout << j << ' ';
+            std::cout << symbol;
         }
         std::cout << '\n';
     }
