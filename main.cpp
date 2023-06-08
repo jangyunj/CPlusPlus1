53d88ea7b741d939234fbfd49e354100dfc2e725
Yun Jang <jang.yunj7@gmail.com>
2023-06-08 17:28:13 -0400

    bank

--- main.cpp
+++ main.cpp
@@ -1,21 +1,50 @@
-// Global var - outside the scope; less secure and pollutes the namespace (try to avoid)
-// Local var - inside the scope
-
 #include <iostream>
+#include <iomanip>
 
-int myNum = 3; // Glocal variable
-
-void printNum();
+void showBalance(double balance);
+double deposit();
+double withdraw(double balance);
 
 int main()
 {
-    int myNum = 2;
-    std::cout << ::myNum << '\n'; // Scope resolution operator -- makes global var take precedence
-    printNum();
-}
+    double balance = 100;
+    int choice = 0;
 
-void printNum()
-{
-    int myNum = 1;
-    std::cout << ::myNum << '\n';
+    do
+    {
+        std::cout << "*****************\n";
+        std::cout << "Enter your choice:\n";
+        std::cout << "*****************\n";
+        std::cout << "1. Show Balance\n";
+        std::cout << "2. Deposit Money\n";
+        std::cout << "3. Withdraw Money\n";
+        std::cout << "4. Exit\n";
+        std::cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            showBalance(balance);
+            break;
+        case 2:
+            balance += deposit();
+            break;
+        case 3:
+            balance -= withdraw(balance);
+            break;
+        case 4:
+            std::cout << "Thanks for visiting!\n";
+            break;
+        default:
+            std::cout << "Invalid choice\n";
+        }
+    } while (choice != 4);
 }
+void showBalance(double balance)
+{
+    std::cout << "Your balance is: $" << std::setprecision(2) << std::fixed << balance << '\n';
+}
+double deposit()
+{
+}
+double withdraw(double balance) {}
