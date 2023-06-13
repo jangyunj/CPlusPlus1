9e575088aa903c2ae978918db1005eeb7ac25cf9
Yun Jang <jang.yunj7@gmail.com>
2023-06-12 22:36:00 -0400

    sizeof2

--- main.cpp
+++ main.cpp
@@ -2,16 +2,10 @@
 
 int main()
 {
-    // Sizeof - determines the size in bytes of a variable, data type, class, objects etc.
+    std::string students[] = {"Bob", "Sandy", "Pat", "Sam"};
 
-    double gpa = 2.5;                // double - 8 bytes
-    std::string name = "Bro";        // String is reference type; 32 bytes
-    char grade = 'A';                // char - 1 byte
-    bool student = true;             // bool - 1 byte
-    char grades[] = {'A', 'B', 'C'}; // 3 bytes
-    std::string students[] = {"Spongebob", "Patrick", "Squidward", "Sandy"};
-
-    std::cout << sizeof(grades) << " bytes\n";
-    std::cout << sizeof(grades) / sizeof(char) << " elements\n"; // Finds the # of elements
-    std::cout << sizeof(students) / sizeof(std::string) << " elements\n";
+    for (int i = 0; i < sizeof(students) / sizeof(std::string); i++)
+    {
+        std::cout << students[i] << '\n';
+    }
 }
