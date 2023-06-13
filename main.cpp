5c27ca8f8b329b082924e84c185c5df9e7d00203
Yun Jang <jang.yunj7@gmail.com>
2023-06-12 22:32:21 -0400

    sizeof

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
-// Array - datastructure containing multiple values
-
 #include <iostream>
 
 int main()
 {
-    double prices[] = {5.00, 7.50, 9.99, 15.00};
-    std::cout << prices[0] << '\n';
-    std::cout << prices[1] << '\n';
-    std::cout << prices[2] << '\n';
-    std::cout << prices[3] << '\n';
+    // Sizeof - determines the size in bytes of a variable, data type, class, objects etc.
+
+    double gpa = 2.5;                // double - 8 bytes
+    std::string name = "Bro";        // String is reference type; 32 bytes
+    char grade = 'A';                // char - 1 byte
+    bool student = true;             // bool - 1 byte
+    char grades[] = {'A', 'B', 'C'}; // 3 bytes
+    std::string students[] = {"Spongebob", "Patrick", "Squidward", "Sandy"};
+
+    std::cout << sizeof(grades) << " bytes\n";
+    std::cout << sizeof(grades) / sizeof(char) << " elements\n"; // Finds the # of elements
+    std::cout << sizeof(students) / sizeof(std::string) << " elements\n";
 }
