d75d3a24af6d315027edd8b5b89ed76c385f16af
Yun Jang <jang.yunj7@gmail.com>
2023-06-12 22:20:27 -0400

    array3

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
+// Array - datastructure containing multiple values
+
 #include <iostream>
 
 int main()
 {
-    std::string cars[3]; // size of array
-
-    cars[0] = "camaro";
-    cars[1] = "camry";
-    cars[2] = "jaguar";
-
-    std::cout << cars[0] << '\n';
-    std::cout << cars[1] << '\n';
-    std::cout << cars[2] << '\n';
+    double prices[] = {5.00, 7.50, 9.99, 15.00};
+    std::cout << prices[0] << '\n';
+    std::cout << prices[1] << '\n';
+    std::cout << prices[2] << '\n';
+    std::cout << prices[3] << '\n';
 }
