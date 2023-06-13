238e9d41ae522f1697a9ebf0de428187c1a37f2b
Yun Jang <jang.yunj7@gmail.com>
2023-06-12 22:37:54 -0400

    sizeof3

--- main.cpp
+++ main.cpp
@@ -2,10 +2,16 @@
 
 int main()
 {
-    std::string students[] = {"Bob", "Sandy", "Pat", "Sam"};
+    // std::string students[] = {"Bob", "Sandy", "Pat", "Sam"};
 
-    for (int i = 0; i < sizeof(students) / sizeof(std::string); i++)
+    // for (int i = 0; i < sizeof(students) / sizeof(std::string); i++)
+    // {
+    //     std::cout << students[i] << '\n';
+    // }
+
+    char grades[] = {'A', 'B', 'C', 'D', 'F'};
+    for (int i = 0; i < sizeof(grades) / sizeof(char); i++)
     {
-        std::cout << students[i] << '\n';
+        std::cout << grades[i];
     }
 }
