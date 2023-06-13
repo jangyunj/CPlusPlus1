f5dc8c0c1adfbff1ab83b2481055dc51dde43893
Yun Jang <jang.yunj7@gmail.com>
2023-06-12 22:15:33 -0400

    array

--- main.cpp
+++ main.cpp
@@ -1,69 +1,10 @@
 #include <iostream>
-#include <ctime>
-
-char getUserChoice();
-char getComputerChoice();
-void showChoice(char choice);
-void chooseWinner(char player, char computer);
 
 int main()
 {
-    char player;
-    char computer;
+    std::string car[] = {"corvette", "mustang", "camry"};
 
-    player = getUserChoice();
-    std::cout << "Your choice: ";
-    showChoice(player);
-
-    computer = getComputerChoice();
+    std::cout << car[0] << '\n';
+    std::cout << car[1] << '\n';
+    std::cout << car[2] << '\n';
 }
-
-char getUserChoice()
-{
-    char player;
-    std::cout << "Rock-Paper-Scissors\n";
-    do
-    {
-        std::cout << "Choose one of the following\n";
-        std::cout << "*******************\n";
-        std::cout << "'r' for rock\n";
-        std::cout << "'p' for paper\n";
-        std::cout << "'s' for scissors\n";
-        std::cin >> player;
-
-    } while (player != 'r' && player != 'p' && player != 's');
-    // This prompt will repeat if the user enters something that is NOT r, p, s
-
-    std::cout << player;
-}
-char getComputerChoice()
-{
-    srand(time(0));
-    int num = rand() % 3 + 1;
-
-    switch (num)
-    {
-    case 1:
-        return 'r';
-    case 2:
-        return 'p';
-    case 3:
-        return 's';
-    }
-}
-void showChoice(char choice)
-{
-    switch (choice)
-    {
-    case 'r':
-        std::cout << "Rock\n";
-        break;
-    case 'p':
-        std::cout << "Paper\n";
-        break;
-    case 's':
-        std::cout << "Scissors\n";
-        break;
-    }
-}
-void chooseWinner(char player, char computer) {}
