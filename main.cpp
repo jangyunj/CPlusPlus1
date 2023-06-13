800e6281163b048ecbfa6f6e859b8d0f7de0e220
Yun Jang <jang.yunj7@gmail.com>
2023-06-12 22:42:09 -0400

    foreach

--- main.cpp
+++ main.cpp
@@ -2,16 +2,19 @@
 
 int main()
 {
-    // std::string students[] = {"Bob", "Sandy", "Pat", "Sam"};
-
-    // for (int i = 0; i < sizeof(students) / sizeof(std::string); i++)
-    // {
-    //     std::cout << students[i] << '\n';
-    // }
+    // Foreach - loop that eases the traversal over an iterable data set
+    //         - less flexible
 
     char grades[] = {'A', 'B', 'C', 'D', 'F'};
-    for (int i = 0; i < sizeof(grades) / sizeof(char); i++)
+
+    for (char grade : grades)
     {
-        std::cout << grades[i];
+        std::cout << grade << '\n';
+    }
+
+    int numbers[] = {10, 20, 30, 40};
+    for (int number : numbers)
+    {
+        std::cout << number << '\n';
     }
 }
