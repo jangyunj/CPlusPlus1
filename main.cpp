2368af554c371053877f2dc411dd4bd71faa0fbd
Yun Jang <jang.yunj7@gmail.com>
2023-06-16 02:16:31 -0400

    memoryAdd

--- main.cpp
+++ main.cpp
@@ -2,19 +2,11 @@
 
 int main()
 {
-    std::string foods[5];
-    int size = sizeof(foods) / sizeof(std::string);
+    std::string name = "bro";
+    int age = 21;
+    bool isStudent = true;
 
-    for (int i = 0; i < size; i++)
-    {
-        std::cout << "Enter food you like #" << i + 1 << ":";
-        std::getline(std::cin, foods[i]);
-    }
-
-    std::cout << "You like the following food: \n";
-
-    for (std::string food : foods)
-    {
-        std::cout << food << '\n';
-    }
+    std::cout << &name << '\n';
+    std::cout << &age << '\n';
+    std::cout << &isStudent << '\n';
 }
