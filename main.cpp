7c4b5b8f3bf72edfebda05a95d92fe94fe06174f
Yun Jang <jang.yunj7@gmail.com>
2023-07-02 23:07:21 -0400

    nullptr

--- main.cpp
+++ main.cpp
@@ -2,19 +2,9 @@
 
 int main()
 {
-    // pointers = variable that stores memory address of another variable
-    //& address of operator
-    //* dereference operator
+    // nullptr - null pointer literal; helpful when determining if address was successfully assigned to the pointer
 
-    std::string name = "Bro";
-    int age = 21;
-    std::string freePizzas[5] = {"pizza1", "pizza2", "pizza3", "pizza4", "pizza5"};
+    int *pointer = nullptr;
+    int x = 123;
 
-    std::string *pName = &name; // pointer is pointing to the name address variable
-    int *pAge = &age;
-    std::string *pFreePizzas = freePizzas;
-
-    std::cout << *pName; // accessing the VALUE at this memory location
-    std::cout << *pAge;
-    std::cout << *pFreePizzas;
-}
+    pointer = &x;
