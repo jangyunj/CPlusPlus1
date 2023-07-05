a7dc5cf1a6ebf896c35d5439259fd11499c41be3
Yun Jang <jang.yunj7@gmail.com>
2023-07-04 21:59:11 -0400

    dynamic memory

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+// Dynamic memory - stores in HEAP rather than stack
+//                - allocated after the program is already compiled and running
+//                - useful when we don't know how much memory we'll need; more flexible
 
 int main()
 {
-    // nullptr - null pointer literal; helpful when determining if address was successfully assigned to the pointer
+    int *pNum = NULL;
+    pNum = new int;
+    *pNum = 21;
 
-    int *pointer = nullptr;
-    int x = 123;
+    std::cout << "address: " << pNum << '\n';
+    std::cout << "value: " << *pNum << '\n';
 
-    pointer = &x;
+    delete pNum; // freeing up the memory at the end
+}
