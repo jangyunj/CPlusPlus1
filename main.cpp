893056cfbeb784283fa84bc5b4a98bc5ea7b4ee4
Yun Jang <jang.yunj7@gmail.com>
2023-07-05 23:59:23 -0400

    template fx

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 template <typename T, typename U>
 
-// function template - compatible with different data types; don't need to write multiple fx
-
-auto max(T x, U y)
+auto min(T x, U y)
 {
-    return x > y ? x : y;
+    return x < y ? x : y;
 }
 
 int main()
 {
-    std::cout << max(3, 4.1) << '\n';
+    std::cout << min(2, 3.1);
 }
