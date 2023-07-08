b232247b07dc45235bc9718a2ab63ab5d2f90942
Yun Jang <jang.yunj7@gmail.com>
2023-07-08 16:54:40 -0400

    pointers2

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ int main()
     int *num1 = new int(11); // new int somewhere in memory with value of 11
     int *num2 = num1;
 
+    *num1 = 22; // points to this integer
     std::cout << num1 << '\n';
     std::cout << num2 << '\n';
+
+    std::cout << *num1 << '\n'; // prints the integer stored in this memory
+    std::cout << *num2 << '\n'; // prints the integer stored in this memory
 }
