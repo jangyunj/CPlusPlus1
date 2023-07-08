8507d80997c3029dabf9a075c0591f0a9c3e416c
Yun Jang <jang.yunj7@gmail.com>
2023-07-08 17:06:00 -0400

    class

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
-// pointers
 #include <iostream>
+
+class Cookie
+{
+private:
+    string color;
+
+public:
+    Cookie(string color)
+    {
+        this->color = color;
+    }
+};
+
+Cookie *cookieOne = new Cookie("green");
+Cookie *cookieTwo = new Cookie("blue");
 
 int main()
 {
-    int *num1 = new int(11); // new int somewhere in memory with value of 11
-    int *num2 = num1;
-
-    *num1 = 22; // points to this integer
-    std::cout << num1 << '\n';
-    std::cout << num2 << '\n';
-
-    std::cout << *num1 << '\n'; // prints the integer stored in this memory
-    std::cout << *num2 << '\n'; // prints the integer stored in this memory
 }
