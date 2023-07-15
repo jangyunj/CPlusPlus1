fb1b76ebabee7012e041f76e7818122834b46f52
Yun Jang <jang.yunj7@gmail.com>
2023-07-14 23:40:37 -0400

    cpa

--- main.cpp
+++ main.cpp
@@ -1,16 +1,9 @@
-// Literals - values such as numbers, characters, or string of characters whose values are self-evident
-// integer literals - decimal, octal, hexadecimal
-// Octal- 0
-// Hexadecimal - Ox, OX
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    int n, m, o;
-    n = 25;
-    m = 031;
-    o = 0x19;
-    std::cout << n << std::endl
-              << m << std::endl
-              << o;
+    int i = 3, j = ++i, k = ++i;
+    cout << k << j << i;
 }
