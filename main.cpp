131aa7aad984b941554bbfe7ddd0690ed34847d4
Yun Jang <jang.yunj7@gmail.com>
2023-07-15 22:46:25 -0400

    cpa5

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
-    float x = 1./2.+2./4.;
-    cout << x;
+   int i = 3, j = 2, k = -1;
+
+   if (i > 0)
+   {
+      if (j <= 0)
+      {
+         if (k < 0)
+            k++;
+         if (k <= 0)
+            k--;
+      }
+      if (j > 0)
+         i++;
+   }
+   if (i <= 0)
+      j++;
+   std::cout << i + j + k;
 }
