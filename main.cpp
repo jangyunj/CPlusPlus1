c3ef3ecb2506c15ce91c1f558b58a4a2881d01ea
Yun Jang <jang.yunj7@gmail.com>
2023-07-15 23:34:32 -0400

    cpa6

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 
 int main()
 {
-   int k = 1 % 2 + 4 % 2; // modulus
+   int i = 3, j, k;
+
+   if (i > 0)
+      j = 2 + i * i;
+   if (j >= 0)
+      k = j % i + 2;
+   if (k >= 0)
+      k = j % i % k; // int k = 11 % 3 % 4;
+
    std::cout << k;
 }
