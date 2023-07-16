d6658317b6a4bfed60b1825dc7c4adda68e832a5
Yun Jang <jang.yunj7@gmail.com>
2023-07-16 19:54:48 -0400

    cpa

--- main.cpp
+++ main.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
-#include <vector>
+
 using namespace std;
 
 int main()
 {
-   vector<string> arr{"apple", "banana", "cherry"};
-   int index = 0;
-   while (index < arr.size())
-   {
-      string element = arr.at(index);
-      cout << element << endl;
-      index++;
-   }
+   int i = 0, j = i++, k = --i;
+   cout << i << j << k;
 }
