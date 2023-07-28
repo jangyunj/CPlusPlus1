629b3d982bfacdfaec865aec7842a8d3835bb140
Yun Jang <jang.yunj7@gmail.com>
2023-07-28 13:55:16 -0400

    struct2

--- main.cpp
+++ main.cpp
@@ -6,12 +6,15 @@ struct Rectangle
 {
    int length;
    int width;
-}; // r1, r2, r3; <- you can also declare variables here
+};
 
 int main()
 {
-   struct Rectangle r1 = {10, 5};
 
-   // printf("Hello world");
-   printf("%d", sizeof(r1)); // length and width are ints and 4 bytes each
+   struct Rectangle r1;
+   r1.length = 10;
+   r1.width = 5;
+
+   cout << r1.length << endl;
+   cout << r1.width << endl;
 }
