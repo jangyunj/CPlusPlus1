3f13e0ef6f30edecb7a356c92c87a4ed986f639c
Yun Jang <jang.yunj7@gmail.com>
2023-08-03 23:41:57 -0400

    array as paramter3

--- main.cpp
+++ main.cpp
@@ -2,17 +2,23 @@
 
 using namespace std;
 
-void fun(int *A, int n)
+int *fun(int size)
 {
-   A[0] = 15;
+   int *p;
+   p = new int[size]; // HEAP
+   for (int i = 0; i < size; i++)
+   {
+      p[i] = i + 1;
+      return p;
+   }
 }
+
 int main()
 {
-   int A[] = {2, 4, 6, 8, 10};
-   int n = 5;
-   fun(A, n);
-   for (int x : A)
+   int *ptr, sz = 5;
+   ptr = fun(sz);
+   for (int i = 0; i < sz; i++)
    {
-      cout << x << " ";
+      cout << ptr[i] << endl;
    }
 }
